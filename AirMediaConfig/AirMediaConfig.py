03ab012b0aed14c8bbd8d5f2c74360c862a59ba8
iWumbo19 <rancourtvideo@gmail.com>
2021-11-09 18:29:37 -0500

    Finished hash method for secret. Seperated admin shell from AVS shell to allow a fallback to AVS if admin can't connect. hash is now stored in hardcode

--- AirMediaConfig/AirMediaConfig.py
+++ AirMediaConfig/AirMediaConfig.py
@@ -35,50 +35,21 @@ def GetCSIP():
         print("That is not a valid IPv4 address. Please Try again")
         GetCSIP()
 
-def GetADUsers():
-    print("Type AD enabled usernames. Seperate names with a single comma (no spaces)")
-    adString = input()
-    ADUsers = adString.split(',', -1)
-    return ADUsers
 
 def GetSecret():
-    pwdHash = 'a0c5ad9a06d9a5c003dd94073b71aeaab480f4b2ec5e2de5a1943b291c02e459'
-    salt = uuid.uuid4().hex
+    pwdHash = 'c7c91ea881a7edbcfb2296c1f9b1236e815132fd5bf1cce1e79c88163197cf82'
     print("What is the password for AVServices")
     secret = getpass.getpass(prompt="")
-    hash = hashlib.sha256(secret.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+    hash = hashlib.sha256(secret.encode('utf-8')).hexdigest()
     if hash == pwdHash:
+        print("Password Accepted")
         return secret
     else:
         print("That password is incorrect")
+        return GetSecret()
 
 
-
-def Main():
-    host = GetIP()
-    port = 22
-    username = "admin"
-    password = "admin"
-    hostname = GetHostname()
-    avuser = "AVServices"
-    secret = GetSecret()
-    csIP = GetCSIP()
-    ADusers = GetADUsers()
-
-
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, port,username, password)
-        print("Logged in with default Credentials")
-    except:
-        print("Default Credentials Failed or 'admin' credentials don't exist")
-        print("The Program Will Now Close... Sorry")
-        input()
-        sys.exit()
-        
-
-
+def AdminStart(ssh, host, port, hostname, avuser, secret, csIP):
     #Adds AVServices User
     try:
         print(f"Adding User {avuser} to device")
@@ -116,11 +87,18 @@ def Main():
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, port,avuser, secret)
         print("Connection back")
+        NoAdmin(ssh, host, port, hostname, avuser, secret, csIP)
     except:
         print(f"Something failed when trying to reconnect with {avuser} account")
+        print(f"Shutting Down")
+        input()
+        sys.exit
 
 
 
+
+
+def NoAdmin(ssh, host, port, hostname, avuser, secret, csIP):
     #Attempts to delete admin
     try:
         print(f"Deleting Unsecure Admin Account")
@@ -158,31 +136,45 @@ def Main():
         lines = stdout.readlines()
         print(lines[0])
     except:
-        print(stderr)
-        
-        
-    #Adds all users to AD
-    print("Adding users to ad.unh.edu AD")
-    for name in ADusers:
-        try:        
-            print(f"Adding {name}")
-            stdin, stdout, stderr = ssh.exec_command(f"addusert -N:ad.unh.edu\{name} -G:Administrators")
-            lines = stdout.readlines()
-            print(lines[0])
-        except:
-            print(stderr)    
+        print(stderr)  
     
             
     print(f"Script Complete")
     input()
 
 
-def HashMaker():
-    
 
-    
-HashMaker()
+
+def Main():
+    host = GetIP()
+    port = 22
+    username = "admin"
+    password = "admin"
+    hostname = GetHostname()
+    avuser = "AVServices"
+    secret = GetSecret()
+    csIP = GetCSIP()
+
+
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, port,username, password)
+        print("Logged in with default Credentials")
+    except:
+        print("Default Credentials Failed or 'admin' credentials don't exist")
+        print("Trying default AVServices")
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(host, port, avuser, secret)
+            print("Logged in with AVServices account")
+            NoAdmin(ssh, host, port, hostname, avuser, secret, csIP)
+        except:
+            print("Couldn't log in with any default credentials. The program will now close")
+            input()
+            sys.exit()
 
 
 #Program Entrance    
-#Main()
+Main()
